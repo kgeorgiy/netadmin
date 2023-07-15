6c6543b4557a9be560a5201ed93ef928d9ce1e5e
Georgiy Korneev <kgeorgiy@kgeorgiy.info>
2023-07-15 12:08:58 +0300

    * Paths fixed

--- netadmin_minion/scripts/deb-install.sh
+++ netadmin_minion/scripts/deb-install.sh
@@ -6,10 +6,10 @@ SCRIPT=/etc/init.d/$NAME
 BASE="$(dirname "${BASH_SOURCE[0]}")/.."
 
 cp "$BASE/scripts/deb-service.sh" "$SCRIPT"
-cp "$BASE/target/release/netadmin_minion" "/bin/$NAME"
+cp "$BASE/../target/release/netadmin-minion" "/bin/$NAME"
 
 mkdir -p "/etc/$NAME"
-cp "$BASE/resources/minion.yaml" \
+cp "$BASE/resources/netadmin-minion.yaml" \
    "$BASE/__keys/netadmin-server.crt" \
    "$BASE/__keys/netadmin-minion.crt" \
    "$BASE/__keys/netadmin-minion.key" \
